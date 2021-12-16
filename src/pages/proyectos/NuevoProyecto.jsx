bebe5ee413012bd880432bf70838b480872fd8f0
Petitevale <vrestrepol@unal.edu.co>
2021-12-15 19:55:48 -0500

    Se termina crear Proyecto

--- src/pages/proyectos/NuevoProyecto.jsx
+++ src/pages/proyectos/NuevoProyecto.jsx
@@ -8,10 +8,9 @@ import PrivateRoute from 'components/PrivateRoute';
 import ButtonLoading from 'components/ButtonLoading';
 import { nanoid } from 'nanoid';
 import { Enum_TipoObjetivo } from 'utils/enums';
-// import { CreateObjectiveContext } from 'context/createObjectiveContext';
-//import { useCreateObjective} from 'context/createObjetiveContext'
 import useFormData from 'hooks/useFormData';
 import { CREAR_PROYECTO } from 'graphql/proyectos/mutations';
+import { ObjContext, useObj } from 'context/objContext';
 
 const NuevoProyecto = () => {
   const {form, formData, updateFormData } = useFormData();
@@ -22,26 +21,41 @@ const NuevoProyecto = () => {
     }
   });
   
+  const [crearProyecto, {data: mutationData, loading: mutationLoading, error:mutationError}] = 
+  useMutation(CREAR_PROYECTO)
+
+
   useEffect(() =>{
     console.log(data);
     if(data){
       const lu = {}
-      data.Usuarios.forEach(elemento =>{
+      data.Usuarios.forEach((elemento) =>{
         lu[elemento._id] = elemento.correo;
       });
       setListaUsuarios(lu)
     }
   },[data]);
 
+  useEffect(()=>{
+    console.log("data mutation", mutationData)
+  })
+
   const submitForm = (e)=>{
     e.preventDefault();
-    console.log(formData);
+    
+    formData.objetivos = Object.values(formData.objetivos);
+    formData.presupuesto = parseFloat(formData.presupuesto);
+    
+    crearProyecto({
+      variables: formData,
+    });
+
   } 
 
   if (loading) return <div>...Cargando</div>
 
     return (
-        // <PrivateRoute roleList={['ADMINISTRADOR', 'LIDER']}>
+        <PrivateRoute roleList={['ADMINISTRADOR', 'LIDER']}>
           <div className='p-10 flex items-center flex-col'>
               <div className='self-start' >
                 <Link to='/proyectos'>
@@ -57,13 +71,76 @@ const NuevoProyecto = () => {
               <Input label='Fecha de Inicio' name='fechaInicio' type='date' required={true} />
               <Input label='Fecha de Fin' name='fechaFin' type='date' required={true} />
               <DropDown label='Líder' name='lider' required={true} options={listaUsuarios} />
-              {/* <Objetivos /> */}
+              <Objetivos />
               <ButtonLoading loading={false} disabled={false} text='Crear Proyecto' />
             </form>
           </div>
-        // </PrivateRoute>
+        </PrivateRoute>
       );
     };
+
+const Objetivos = () =>{
+
+  const [listaObjetivos, setListaObjetivos] = useState([]);
+  const [maxObjetivos, setMaxObjetivos] = useState(false)
+
+  const eliminarObjetivo = (id) => {
+    setListaObjetivos(listaObjetivos.filter((el) => el.props.id !== id));
+  };
+
+  const componenteObjetivoAgregado = () => {
+    const id = nanoid();
+    return <FormObjetivo key={id} id={id}/>;
+  }
+
+  useEffect(() =>{
+    if (listaObjetivos.length > 4){
+      setMaxObjetivos(true);
+    }
+    else{
+      setMaxObjetivos(false);
+    }
+  }, [listaObjetivos]);
+  return(
+    <ObjContext.Provider value={{ eliminarObjetivo }}>
+      <div>
+        <span>Objetivos del Proyecto</span>
+        {!maxObjetivos && (
+            <i 
+            onClick={() => setListaObjetivos([...listaObjetivos, componenteObjetivoAgregado()])}
+            className='fas fa-plus rounded-full bg-moradoClaro-dark hover:bg-moradoClaro-default text-white p-2 mx-2 cursor-pointer'
+            />
+          )}
+
+        {listaObjetivos.map((objetivo)=> {
+          return objetivo;
+        })}
+      </div>
+    </ObjContext.Provider>
+  );
+};
+
+const FormObjetivo = ({ id })=> {
+  const {eliminarObjetivo} = useObj();
+  return(
+    <div className='flex items-center'> 
+      <Input 
+      name={`nested||objetivos||${id}||descripcion`} 
+      label='Descripción' 
+      type='text' 
+      required={true} 
+      />
+      <DropDown 
+      name={`nested||objetivos||${id}||tipo`}
+      options={Enum_TipoObjetivo} 
+      label='Tipo de Objetivo' 
+      required={true}
+      />
+      <i 
+      onClick={()=> eliminarObjetivo(id)} className='fas fa-minus rounded-full bg-moradoClaro-dark hover:bg-moradoClaro-default text-white p-2 mx-2 cursor-pointer mt-6'/>
+    </div>
+  )
+}
 
 export default NuevoProyecto
 
